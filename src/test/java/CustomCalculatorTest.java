a2bbb2e5fe9f900c2c3269d46663e9778a5a96d2
yeongin <pyi7628@gmail.com>
2019-07-17 00:31:02 +0900

    divide 메소드에 assertFalse, anyOf 테스트 코드 추가 작성

--- src/test/java/CustomCalculatorTest.java
+++ src/test/java/CustomCalculatorTest.java
@@ -49,11 +49,15 @@ public class CustomCalculatorTest {
     }
 
     //나누기 테스트 작성
+    //Test by 영인
     @Test
     public void divide() {
         customCalculator = new CustomCalculator();
         int result = customCalculator.divide(25,5);
-        /* 이곳에 테스트 코드를 작성하세요. */
+        assertThat(result,is(5));
+        assertFalse(result==10);
+        assertThat(result,not(10));
+        assertThat(result,anyOf(is(10),lessThan(10)));
         System.out.println("result :: " + result);
     }
 }
